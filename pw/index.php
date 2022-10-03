b9907d142d120380af0a6b358c071908cb0ba059
fkryadr <fkryadr@yandex.com>
2022-10-03 13:30:30 +0700

    Menambahkan Framework Materialize

--- pw/index.php
+++ pw/index.php
@@ -10,6 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"></script>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <title>Latihan Prakweb</title>
 </head>
 <body>
