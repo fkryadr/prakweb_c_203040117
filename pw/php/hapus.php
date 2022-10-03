607cbac6dfba221ff014a04db50cdd50331d4b8c
Kriii <fkryadr@yandex.com>
2022-10-03 20:25:25 +0700

    Menambahkan Function Hapus

--- pw/php/hapus.php
+++ pw/php/hapus.php
@@ -1,1 +1,24 @@
+
 <?php
+
+
+require 'function.php';
+$id =$_GET['id'];
+
+
+if(hapus($id) > 0) {
+    echo "<script>
+     alert('Data Berhasil Dihapus');
+     document.location.href = '../index.php';
+ </script>";
+}else {
+    echo "<script>
+             alert('Data Gagal Dihapys');
+             document.location.href = '../index.php';
+        </script>";
+}
+
+
+
+
+?>
