41bb2818561aa9cd1c22767f08f3677b26ad40a2
fkryadr <fkryadr@yandex.com>
2022-10-10 11:19:26 +0700

    Controller

--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -1,11 +1,42 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
 
     public function __construct()
     {
        $url = $this->parseURL();
-       var_dump($url);
+
+        // COntroller
+
+       if( file_exists('../app/controller/' . $url[0] . '.php') ) {
+           $this->controller = $url[0];
+           unset($url[0]);
+       }
+
+        require_once '../app/controller/'. $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+       // Method
+       if( isset($url[1]) ) {
+           if( method_exists($this->controller, $url[1]) ) {
+               $this->method = $url[1];
+               unset($url[1]);
+           }
+       }
+
+       // parameter
+        if ( !empty($url) ) {
+            $this->params = array_values($url);
+        }
+
+        // Menjalankan Controller & Method, kirimkan params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
+
+
     }
 
     public function parseURL() {
