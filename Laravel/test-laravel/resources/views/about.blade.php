19b5b69a994254c1be5a353e717295058e3ce700
Kriii <fkryadr@yandex.com>
2022-10-17 06:44:30 +0700

    Struktur folder, Routes & View

---
+++ Laravel/test-laravel/resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WPU BLOG !|About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1>Ini Halaman About</h1>
+<h3><?= $name; ?></h3>
+<p><?= $email; ?></p>
+<img src="img/<?= $image; ?>" alt="<?= $name?>" width="200px">
+
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
