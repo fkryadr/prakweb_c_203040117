5713c5b7abbdee9757c0ad20fb32d55f07b316d9
Kriii <fkryadr@yandex.com>
2022-10-17 09:28:14 +0700

    Blade Templating Engine

--- Laravel/test-laravel/resources/views/posts.blade.php
+++ Laravel/test-laravel/resources/views/posts.blade.php
@@ -1,18 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPU BLOG !|Posts</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
 <h1>Ini Halaman Posts</h1>
+@endsection
 
 
 
-<script src="js/script.js"></script>
-</body>
-</html>
+
+
--- Laravel/test-laravel/routes/web.php
+++ Laravel/test-laravel/routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'title' => 'Home'
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => 'About',
         "name" => "Fikri Adrian",
         "email" => "fkryadr@yandex.com",
         "image" => "okky.jpg"
@@ -26,5 +29,6 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts',['title' => 'Posts'
+    ]);
 });
